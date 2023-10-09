e933c4c884e2826c62a222c908b4f9ba26bf5099
Ishaan Desai <desaii@gl-login2.arc-ts.umich.edu>
2023-10-09 16:42:01 -0400

    Refactoring functionality on macro side into different files, and using first-participant method for setting time window size of the coupling. Macro side is not the first participant

--- macro_laminate/VUMAT.f
+++ macro_laminate/VUMAT.f
@@ -1,4 +1,6 @@
-! Authors: Minh Hoang Nguyen (mhoangn) and Ishaan Desai (desaii)
+      include './get_strains.f'
+      include './material_model.f'
+! Authors: Ishaan Desai (desaii)
       subroutine vumat(
 ! Read only (unmodifiable)variables -
      1     nblock, ndir, nshr, nstatev, nfieldv, nprops, lanneal,
@@ -34,12 +36,19 @@
 
       ! preCICE variables
       integer :: rank, size, ongoing, dimensions, bool
-      double precision :: preCICE_dt
+      double precision :: preCICE_dt, abaqus_dt ! We cannot change the dt variable
       double precision, dimension(nblock*ndir) :: strains1to3, strains4to6,
      * stresses1to3, stresses4to6, couplingVertices
       integer, dimension(nblock) :: vertexIDs
 
       write(*,*) "t = ", totalTime, ",", " dt = ", dt, " VUMAT: Entering VUMAT."
+
+      ! When not in initialization, vertexIDs of preCICE are defined
+      ! manually, because VUMAT cannot hold global data.
+      ! TODO: Need to find a way to store global data in VUMAT
+      do k = 0, nblock - 1
+         vertexIDs(k + 1) = k
+      end do
 
       if (dt /= 1) then
          if (totalTime < 2.*dt) then ! only the first step
@@ -113,13 +122,6 @@
 
          end if ! if (totalTime < 2.*dt)
 
-         ! When not in initialization, vertexIDs of preCICE are defined
-         ! manually, because VUMAT cannot hold global data.
-         ! TODO: Need to find a way to store global data in VUMAT
-         do k = 0, nblock - 1
-            vertexIDs(k + 1) = k
-         end do
-
          ! Check if coupling is still going on
          call precicef_is_coupling_ongoing(bool)
          !call assert(bool.eq.1)
@@ -154,7 +156,11 @@
 
 ! ==========================================================================
 
-         call precicef_advance(dt)
+         call precicef_get_max_time_step_size(preCICE_dt)
+         abaqus_dt = min(preCICE_dt, dt)
+
+         call precicef_advance(abaqus_dt)
+
          write(*,*) "(t = ", totalTime, ") VUMAT: Coupling has been advanced."
 
 ! Read stresses from preCICE and apply them ===============================
@@ -223,124 +229,3 @@
 
       return
       end ! Subroutine vumat
-
-
-
-      subroutine get_strains(
-     *   nblock, ndir, nshr, nstatev,
-     *   strainInc, stateOld, state, strains)
-         ! Orientation of strains: [11, 22, 33, 12, 23, 13]
-
-         integer, intent(in) :: nblock, ndir, nshr, nstatev
-         real*8, dimension(nblock, nstatev), intent(in) :: stateOld
-         real*8, dimension(nblock, ndir + nshr), intent(in) :: strainInc
-
-         real*8, dimension(nblock, nstatev), intent(out) :: state
-         real*8, dimension(nblock, ndir + nshr), intent(out) :: strains
-
-         real*8, dimension(ndir + nshr) :: strain
-         integer :: k, d
-
-         parameter(i_sdv_eps11=1,
-     *    i_sdv_eps22 = 2,
-     *    i_sdv_eps33 = 3,
-     *    i_sdv_gamma12 = 4,
-     *    i_sdv_gamma23 = 5,
-     *    i_sdv_gamma13 = 6,
-     *    i_sdv_active = 7,
-     *    i_sdv_t_f = 8)
-
-         state(:, i_sdv_eps11) = zero
-         state(:, i_sdv_eps22) = zero
-         state(:, i_sdv_eps33) = zero
-         state(:, i_sdv_gamma12) = zero
-         state(:, i_sdv_gamma23) = zero
-         state(:, i_sdv_gamma13) = zero
-         state(:, i_sdv_active) = one
-         state(:, i_sdv_t_f) = zero
-
-         ! Loop through material points to collect strains
-         do k = 1, nblock
-            state(k, :) = stateOld(k, :)
-
-            strain(1) = state(k, i_sdv_eps11) + strainInc(k, 1)
-            strain(2) = state(k, i_sdv_eps22) + strainInc(k, 2)
-            strain(3) = state(k, i_sdv_eps33) + strainInc(k, 3)
-            strain(4) = state(k, i_sdv_gamma12) + two*strainInc(k, 4)
-            strain(5) = state(k, i_sdv_gamma23) + two*strainInc(k, 5)
-            strain(6) = state(k, i_sdv_gamma13) + two*strainInc(k, 6)
-            state(k, i_sdv_eps11:i_sdv_gamma13) = strain
-
-            do d = 1, ndir + nshr
-
-               strains(k, d) = strain(d)
-
-            end do ! ndir + nshr
-
-         end do ! nblock
-
-      return
-      end ! subroutine get_strains
-
-
-
-      subroutine solve_material_model(
-     *   nblock, ndir, nshr, nprops,
-     *   props, strains, stresses)
-      ! Orientation of stresses: [11, 22, 33, 12, 23, 13]
-
-      parameter(zero=0.d0, half=0.5d0, one=1.d0, two=2.d0, three=3.d0)
-
-      parameter(i_prp_E11=1,
-     * i_prp_E22 = 2,
-     * i_prp_G12 = 3,
-     * i_prp_nu12 = 4,
-     * i_prp_nu23 = 5)
-
-      integer, intent(in) :: nblock, ndir, nshr, nprops
-      real*8, dimension(nprops), intent(in) :: props
-      real*8, dimension(nblock, ndir + nshr), intent(in) :: strains
-
-      real*8, dimension(nblock, ndir + nshr), intent(out) :: stresses
-
-      real*8, dimension(ndir + nshr, ndir + nshr) :: Q
-      real*8 :: E11, E220, E33, nu12, nu23, nu13, G13, G23, nu31, nu21
-      real*8 :: temp
-
-      ! Initializations for stress calculation
-      E11  = props(i_prp_E11)
-      E22  = props(i_prp_E22)
-      E33  = props(i_prp_E22)
-      nu12 = props(i_prp_nu12)
-      nu23 = props(i_prp_nu23)
-      nu13 = props(i_prp_nu12)
-      G12  = props(i_prp_G12)
-      G13  = props(i_prp_G12)
-      G23  = props(i_prp_E22)/2./(1.+props(i_prp_nu23))
-      nu31 = nu13*E33/E11
-      nu21 = nu12*E22/E11
-      nu31 = nu13*E33/E11
-      nu32 = nu23*E33/E22
-
-      ! Calculate stresses using a material model
-      temp   = nu12*nu21 + nu23*nu32 + nu13*nu31 + 2.*nu21*nu32*nu13
-      Q      = zero
-      Q(1,1) = (1.-nu23*nu32)*E11 / (1.-temp)
-      Q(2,2) = (1.-nu13*nu31)*E22 / (1.-temp)
-      Q(3,3) = (1.-nu12*nu21)*E33 / (1.-temp)
-      Q(1,2) = (nu21+nu31*nu23)*E11/(1.-temp)
-      Q(2,1) = Q(1,2)
-      Q(1,3) = (nu31+nu21*nu32)*E11/(1.-temp)
-      Q(3,1) = Q(1,3)
-      Q(2,3) = (nu32+nu12*nu31)*E22/(1.-temp)
-      Q(3,2) = Q(2,3)
-      Q(4,4) = G12
-      Q(5,5) = G23
-      Q(6,6) = G13
-
-      do k = 1, nblock
-         stresses(k, :) = MATMUL(Q, strains(k, :))
-      end do
-
-      return
-      end ! Subroutine solve_material_model
