11f4e38b166a59e25c9504df44864ffd6d1f2e7c
Hoang Nguyen <115256661+mhoangnUM@users.noreply.github.com>
2023-10-17 15:20:56 -0400

    Add first prototype micro model Abaqus files  (#2)
    * Uploaded initial codes to build RUCs
    
    * adjusted gitignore
    
    * Established RUC multi-strain loading and restart-import input files
    
    * Added and tested incremental input files
    
    original: deps22
    Increment n-1 (nm1): dgamma21
    Increment n (n): deps11, deps33
    
    * Added postprocessing script to compute stresses from RUC
    
    * Added postprocessing script to compute stresses from RUC
    
    RUC dimensions are hard-coded for now, assuming same sizes for all RUCs.
    
    * Revert "Added postprocessing script to compute stresses from RUC"
    
    This reverts commit 798da928f668406e2825d1d2467c82fefeb33817.
    
    * Added parameterized input files to apply global strains
    
    * Adding skeleton code for connecting the Python class MicroSimulation to Abaqus RUC
    
    * Add micro files for restart method of running RUCs
    
    * Set correct micro simulation file in the Micro Manager configuration
    
    * File renaming and restructuring
    
    * Code to launch multiple RUC jobs
    
    * Remove RUC job submission scripts as we are now launching the RUC jobs directly from the terminal
    
    * Added RUC with VUAMP
    
    * Resturcture micro RUC problems in three categories, according to the solution strategy
    
    * Add cleaning script for every RUC folder, use subprocess for all system calls, check if .odb file exists before accessing it
    
    * Remove unnecessary folder and format Python code
    
    * Adjusted the macro model: Linear amplitude and larger time increment
    
    * Update Crossply_macro_18elements.inp
    
    * First working prototype
    
    * Update Crossply_macro_18elements.inp dt=1e-5
    
    * Remove old build script, and no logging for ABAQUS jobs
    
    * Renaming macro files
    
    * Use meso scale instead of macro scale
    
    * Rename job submission script
    
    * Use meso instead of macro in all places
    
    ---------
    
    Co-authored-by: Ishaan Desai <ishaandesai@gmail.com>
    Co-authored-by: Ishaan Desai <desaii@gl-login3.arc-ts.umich.edu>
    Co-authored-by: Ishaan Desai <desaii@gl-login1.arc-ts.umich.edu>
    Co-authored-by: Ishaan Desai <desaii@gl-login2.arc-ts.umich.edu>

--- micro_ruc/analytical_method/ruc_analytical.py
+++ micro_ruc/analytical_method/ruc_analytical.py
@@ -8,12 +8,12 @@ import numpy as np
 
 class MicroSimulation:
 
-    def __init__(self):
+    def __init__(self, sim_id):
         """
         Constructor of MicroSimulation class.
         """
         self._dims = 3
-        self._state = None
+        self._sim_id = sim_id
 
         # Parameters for material model written by Minh Hoang Nguyen
         self._E11 = 1.6e5
@@ -24,13 +24,13 @@ class MicroSimulation:
         self._nu13 = self._nu12
         self._G12 = 5.7e3
         self._G13 = self._G12
-        self._G23  = self._E22 / 2.0 / (1.0 + self._nu23)
+        self._G23 = self._E22 / 2.0 / (1.0 + self._nu23)
         self._nu31 = self._nu13 * self._E33 / self._E11
         self._nu21 = self._nu12 * self._E22 / self._E11
         self._nu31 = self._nu13 * self._E33 / self._E11
         self._nu32 = self._nu23 * self._E33 / self._E22
 
-        self._Q = np.zeros((6,6))
+        self._Q = np.zeros((6, 6))
 
     def solve(self, strains, dt):
         assert dt != 0
@@ -41,22 +41,23 @@ class MicroSimulation:
             strain_values[i + 3] = strains["strains4to6"][i]
 
         # Material model from VUMAT.f written by Minh Hoang Nguyen
-        temp = self._nu12 * self._nu21 + self._nu23 * self._nu32 + self._nu13 * self._nu31 + 2.0 * self._nu21 * self._nu32 * self._nu13
+        temp = self._nu12 * self._nu21 + self._nu23 * self._nu32 + \
+            self._nu13 * self._nu31 + 2.0 * self._nu21 * self._nu32 * self._nu13
 
-        self._Q[:,:] = 0
+        self._Q[:, :] = 0
 
-        self._Q[0,0] = (1.0 - self._nu23 * self._nu32) * self._E11 / (1.0 - temp)
-        self._Q[1,1] = (1.0 - self._nu13 * self._nu31) * self._E22 / (1.0 - temp)
-        self._Q[2,2] = (1.0 - self._nu12 * self._nu21) * self._E33 / (1.0 - temp)
-        self._Q[0,1] = (self._nu21 + self._nu31 * self._nu23) * self._E11 / (1.0 - temp)
-        self._Q[1,0] = self._Q[0,1]
-        self._Q[0,2] = (self._nu31 + self._nu21 * self._nu32) * self._E11 / (1.0 - temp)
-        self._Q[2,0] = self._Q[0,2]
-        self._Q[1,2] = (self._nu32 + self._nu12 * self._nu31) * self._E22 / (1.0 - temp)
-        self._Q[2,1] = self._Q[1,2]
-        self._Q[3,3] = self._G12
-        self._Q[4,4] = self._G23
-        self._Q[5,5] = self._G13
+        self._Q[0, 0] = (1.0 - self._nu23 * self._nu32) * self._E11 / (1.0 - temp)
+        self._Q[1, 1] = (1.0 - self._nu13 * self._nu31) * self._E22 / (1.0 - temp)
+        self._Q[2, 2] = (1.0 - self._nu12 * self._nu21) * self._E33 / (1.0 - temp)
+        self._Q[0, 1] = (self._nu21 + self._nu31 * self._nu23) * self._E11 / (1.0 - temp)
+        self._Q[1, 0] = self._Q[0, 1]
+        self._Q[0, 2] = (self._nu31 + self._nu21 * self._nu32) * self._E11 / (1.0 - temp)
+        self._Q[2, 0] = self._Q[0, 2]
+        self._Q[1, 2] = (self._nu32 + self._nu12 * self._nu31) * self._E22 / (1.0 - temp)
+        self._Q[2, 1] = self._Q[1, 2]
+        self._Q[3, 3] = self._G12
+        self._Q[4, 4] = self._G23
+        self._Q[5, 5] = self._G13
 
         stresses = np.matmul(self._Q, strain_values)
 
--- micro_ruc/clean.sh
+++ micro_ruc/clean.sh
@@ -1,5 +1,11 @@
-rm -rfv output/*
+rm -rfv output/
 rm -fv *.log
 rm -rfv __pycache__
 rm -rfv precice-profiling/
 rm -fv .nfs*
+
+echo "Cleaning restart_method"
+cd restart_method/
+sh clean_ruc.sh
+rm -rfv ruc_*/
+rm -rfv __pycache__
