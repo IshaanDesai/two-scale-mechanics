de695c7fa79ed7db66290f28224739efb0a445b9
Ishaan Desai <ishaandesai@gmail.com>
2024-03-01 09:41:22 +0530

    [WIP] Further implementation of the meso dummy solver

--- meso_dummy/dummy.py
+++ meso_dummy/dummy.py
@@ -27,9 +27,8 @@ mesh_name = 'laminate-mesh-mesh'
 # RVE_ID = Unique ID for each macro gauss point [INTEGER]
 # MOD_ID = ID for the micromechanical model [INTEGER]
 # RUC_SIZE = Problem size of the RUC model [INTEGER]
-write_data_names = ['RVE_ID', 'MOD_ID', 'RUC_SIZE', 'strains']
-
-read_data_name = 'stresses'
+write_data_names = ['RVE_ID', 'MOD_ID', 'RUC_SIZE', 'strains1to3', 'strains4to6']
+read_data_names = ['stresses1to3', 'stresses4to6']
 
 num_vertices = 10  # Number of vertices
 
@@ -52,11 +51,15 @@ for x in range(num_vertices):
     for y in range(participant.get_mesh_dimensions(mesh_name)):
         vertices[x, y] = x
 
-    for y in range(participant.get_data_dimensions(mesh_name, read_data_name)):
-        read_data[x, y] = x
+for name in read_data_names:
+    for x in range(num_vertices):
+        for y in range(participant.get_data_dimensions(mesh_name, name)):
+            read_data[name][x, y] = x
 
-    for y in range(participant.get_data_dimensions(mesh_name, write_data_name)):
-        write_data[x, y] = x
+for name in write_data_names:
+    for x in range(num_vertices):
+        for y in range(participant.get_data_dimensions(mesh_name, name)):
+            write_data[name][x, y] = x
 
 vertex_ids = participant.set_mesh_vertices(mesh_name, vertices)
 
@@ -67,11 +70,14 @@ while participant.is_coupling_ongoing():
         print("DUMMY: Writing iteration checkpoint")
 
     dt = participant.get_max_time_step_size()
-    read_data = participant.read_data(mesh_name, read_data_name, vertex_ids, dt)
+
+    for name in read_data_names:
+        read_data = participant.read_data(mesh_name, name, vertex_ids, dt)
 
     write_data = read_data + 1
 
-    participant.write_data(mesh_name, write_data_name, vertex_ids, write_data)
+    for name in write_data_names:
+        participant.write_data(mesh_name, name, vertex_ids, write_data[name])
 
     print("DUMMY: Advancing in time")
     participant.advance(dt)
@@ -80,4 +86,4 @@ while participant.is_coupling_ongoing():
         print("DUMMY: Reading iteration checkpoint")
 
 participant.finalize()
-print("DUMMY: Closing python solver dummy...")
+print("DUMMY: Closing meso solver dummy...")
