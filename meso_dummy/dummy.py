1f66912125b577dd5612b0ec24b76484a35dae39
Ibrahim Kaleel <ibrahim.mit@gmail.com>
2024-04-16 10:44:38 -0400

    Additional data for exchange

--- meso_dummy/dummy.py
+++ meso_dummy/dummy.py
@@ -6,48 +6,45 @@ import numpy as np
 import precice
 
 mesh_name = 'dummy-mesh'
+mod_id = 103
+ruc_size = 16
+num_vertices = 5  # Number of vertices
 
-# RVE_ID = Unique ID for each macro gauss point [INTEGER]
-# MOD_ID = ID for the micromechanical model [INTEGER]
-# RUC_SIZE = Problem size of the RUC model [INTEGER]
-write_data_names = ['RVE_ID', 'MOD_ID', 'RUC_SIZE', 'strains1to3', 'strains4to6']
-read_data_names = ['stresses1to3', 'stresses4to6']
 
-num_vertices = 10  # Number of vertices
+# Data names
+write_data_names = ['rve_id', 'mod_id', 'ruc_size', 'strains1to3', 'strains4to6']
+read_data_names = ['stresses1to3', 'stresses4to6','cmat1', 'cmat2', 'cmat3', 'cmat4', 'cmat5', 'cmat6', 'cmat7', 'conv']
 
+# Creating precice participant
 participant = precice.Participant('Meso-scale-dummy', '../precice-config-nasmat-scaling.xml', 0, 1)
-
 assert (participant.requires_mesh_connectivity_for(mesh_name) is False)
 
+# Initializing vertices
 vertices = np.zeros((num_vertices, participant.get_mesh_dimensions(mesh_name)))
+for x in range(num_vertices):
+    vertices[x, 0] = float(x)/float(num_vertices)
+vertex_ids = participant.set_mesh_vertices(mesh_name, vertices)
 
-read_data = dict()
-for name in read_data_names:
-    read_data[name] = np.zeros((num_vertices, participant.get_data_dimensions(mesh_name, name)))
-
+# Initializing write data
 write_data = dict()
 for name in write_data_names:
     write_data[name] = np.zeros((num_vertices, participant.get_data_dimensions(mesh_name, name)))
 
 for x in range(num_vertices):
-    for y in range(participant.get_mesh_dimensions(mesh_name)):
-        vertices[x, y] = x
+    write_data['rve_id'][x, 0] = x+1
+    write_data['mod_id'][x, 0] = mod_id
+    write_data['ruc_size'][x, 0] = ruc_size
+    write_data['strains1to3'][x, :] = np.random.uniform(0, 1e-4, 3) # Random strain values
+    write_data['strains4to6'][x, :] = np.random.uniform(0, 1e-6, 3) # Random strain values
 
+
+# Initializing read data
+read_data = dict()
 for name in read_data_names:
-    for x in range(num_vertices):
-        for y in range(participant.get_data_dimensions(mesh_name, name)):
-            read_data[name][x, y] = x
+    read_data[name] = np.zeros((num_vertices, participant.get_data_dimensions(mesh_name, name)))
 
-for name in write_data_names:
-    for x in range(num_vertices):
-        for y in range(participant.get_data_dimensions(mesh_name, name)):
-            write_data[name][x, y] = x
-
-vertex_ids = participant.set_mesh_vertices(mesh_name, vertices)
 
 participant.initialize()
-
-data_checkpoint = None
 
 print("Starting meso solver dummy...")
 
@@ -57,14 +54,15 @@ while participant.is_coupling_ongoing():
     for name in write_data_names:
         participant.write_data(mesh_name, name, vertex_ids, write_data[name])
 
-    print("Advancing in time")
     participant.advance(dt)
 
-    for name in read_data_names:
-        read_data[name] = participant.read_data(mesh_name, name, vertex_ids, dt)
+    if (participant.get_max_time_step_size()>0.0):
+        for name in read_data_names:
+            read_data[name] = participant.read_data(mesh_name, name, vertex_ids, dt)
 
-    print("Read data at the end of the time step")
-    print(read_data)
+        print("Read data at the end of the time step")
+        print(read_data['cmat6'])
+
 
 
 participant.finalize()
