c98b3370aac417cee98124e896dfd94754eb8412
Ishaan Desai <ishaandesai@gmail.com>
2025-04-14 16:18:56 +0200

    Rename FreeCAD input file to notch

--- meso_ccx_notch/run.sh
+++ meso_ccx_notch/run.sh
@@ -7,9 +7,6 @@ cp config_for_fans.yml config.yml
 export OMP_NUM_THREADS=1
 export CCX_NPROC_EQUATION_SOLVER=1
 
-$HOME/calculix-adapter/bin/ccx_preCICE -i FEMMeshNetgen -precice-participant Meso-structure
-
-# Debugging
-#gdb -ex run --args $HOME/calculix-adapter/bin/ccx_preCICE -i one_element -precice-participant Meso-structure
+$HOME/calculix-adapter/bin/ccx_preCICE -i notch -precice-participant Meso-structure
 
 rm -rfv config.yml
